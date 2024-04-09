16222b776cbd94874fcbf2726e70a8b9953a56de
Pavel <pasha_babich01@mail.ru>
2024-04-10 00:02:12 +0300

    Fix db connection

--- components/Db.php
+++ components/Db.php
@@ -6,7 +6,7 @@ class Db{
         $paramsPath = ROOT.'/config/db_params.php';
         $params = include($paramsPath);
 
-        $dsn = "mysql:host={$params['host']};dbname={$params['dbname']};port=3307";
+        $dsn = "mysql:host={$params['host']};dbname={$params['dbname']};port=3306";
         $db = new PDO($dsn, $params['user'], $params['password']);
 
         return $db;
